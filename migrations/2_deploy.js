64a39ad96f240d264ecaa43501190e7e661f07c4
Eliott Teissonniere <eliott.teissonniere@gmail.com>
2018-01-18 22:01:29 +0100

    Lint

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,15 +1,15 @@
 var registry = artifacts.require('Registry.sol')
 
 module.exports = async deployer => {
-    // Default fee is 1 ETH
-    const fee = web3.toWei(1, 'ether')
-    
-    await deployer.deploy(registry, fee)
+  // Default fee is 1 ETH
+  const fee = web3.toWei(1, 'ether')
 
-    // Register the registry
-    const deployed = await registry.deployed()
-    await deployed.register(registry.address, "Registry", "https://github.com/DeveloppSoft/ethereum_registry", {value: fee})
+  await deployer.deploy(registry, fee)
 
-    // Get back the ether
-    await deployed.withdrawFees()
+  // Register the registry
+  const deployed = await registry.deployed()
+  await deployed.register(registry.address, 'Registry', 'https://github.com/DeveloppSoft/ethereum_registry', {value: fee})
+
+  // Get back the ether
+  await deployed.withdrawFees()
 }
