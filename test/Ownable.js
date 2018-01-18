703b4c6107a21b8425f0795c1f7440a89b54fae0
Eliott Teissonniere <eliott.teissonniere@gmail.com>
2018-01-18 20:02:07 +0100

    Run eslint

--- test/Ownable.js
+++ test/Ownable.js
@@ -1,37 +1,37 @@
 
-import assertRevert from './helpers/assertRevert';
+import assertRevert from './helpers/assertRevert'
 
-var Ownable = artifacts.require('Ownable.sol');
+var Ownable = artifacts.require('Ownable.sol')
 
 contract('Ownable', function (accounts) {
-  let ownable;
+  let ownable
 
   beforeEach(async function () {
-    ownable = await Ownable.new();
-  });
+    ownable = await Ownable.new()
+  })
 
   it('should have an owner', async function () {
-    let owner = await ownable.owner();
-    assert.isTrue(owner !== 0);
-  });
+    let owner = await ownable.owner()
+    assert.isTrue(owner !== 0)
+  })
 
   it('changes owner after transfer', async function () {
-    let other = accounts[1];
-    await ownable.transferOwnership(other);
-    let owner = await ownable.owner();
+    let other = accounts[1]
+    await ownable.transferOwnership(other)
+    let owner = await ownable.owner()
 
-    assert.isTrue(owner === other);
-  });
+    assert.isTrue(owner === other)
+  })
 
   it('should prevent non-owners from transfering', async function () {
-    const other = accounts[2];
-    const owner = await ownable.owner.call();
-    assert.isTrue(owner !== other);
-    await assertRevert(ownable.transferOwnership(other, { from: other }));
-  });
+    const other = accounts[2]
+    const owner = await ownable.owner.call()
+    assert.isTrue(owner !== other)
+    await assertRevert(ownable.transferOwnership(other, { from: other }))
+  })
 
   it('should guard ownership against stuck state', async function () {
-    let originalOwner = await ownable.owner();
-    await assertRevert(ownable.transferOwnership(null, { from: originalOwner }));
-  });
-});
+    let originalOwner = await ownable.owner()
+    await assertRevert(ownable.transferOwnership(null, { from: originalOwner }))
+  })
+})
