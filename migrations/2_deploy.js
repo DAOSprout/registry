882a66ddb3b76efa3edd3d25ffd0475261ffd253
Eliott Teissonniere <eliott.teissonniere@gmail.com>
2018-01-18 21:51:28 +0100

    Migrations

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,3 +1,15 @@
+var registry = artifacts.require('Registry.sol')
+
 module.exports = async deployer => {
-  console.log('Contracts should be deployed here')
+    // Default fee is 1 ETH
+    const fee = web3.toWei(1, 'ether')
+    
+    await deployer.deploy(registry, fee)
+
+    // Register the registry
+    const deployed = await registry.deployed()
+    await deployed.register(registry.address, "Registry", "https://github.com/DeveloppSoft/ethereum_registry", {value: fee})
+
+    // Get back the ether
+    await deployed.withdrawFees()
 }
